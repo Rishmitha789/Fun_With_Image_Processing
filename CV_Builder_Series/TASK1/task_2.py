a53ce40c05bdf0078306030cc1f60d035accdf56
Rishmitha <125901640+Rishmitha789@users.noreply.github.com>
2024-10-07 19:35:14 +0530

    Delete CV_Builder_Series/TASK1 directory

--- CV_Builder_Series/TASK1/task_2.py
+++
@@ -1,6 +0,0 @@
-import cv2
-path = "C:\\Users\\K.KEERTHNA\\Desktop\\CV_Builders_Series\\task_1\\tom.jpg"
-image=cv2.imread(path)
-cv2.imshow("Output",image)
-cv2.waitKey()
-cv2.destroyAllWindows()
--- CV_Builder_Series/TASK1/task_3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-path = "C:\\Users\\K.KEERTHNA\\Desktop\\CV_Builders_Series\\task_1\\tom.jpg"
-image=cv2.imread(path)
-image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow("Output",image)
-cv2.imshow("Output_gray",image_gray)
-cv2.waitKey()
-cv2.destroyAllWindows()
